b4cba5ea22b00c684d2ab5c13b4c84c63ed182d4
farham2025678-web <farham2025678@gmail.com>
2025-09-08 14:11:14 +0800

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,7 +1,10 @@
 public class Main {
 public static void main(String[] args) {
 System.out.println("Hello world");
-
+String nama = "Farham"
+System.out.println("nama = " + nama) ;
+                  
+  
 }
 
 }
