a36bd0e4c3b9585c310db5336b1086464d1adb7d
farham2025678-web <farham2025678@gmail.com>
2025-09-09 20:41:55 +0800

    Day2.java

--- Day2.java
+++ Day2.java
@@ -2,8 +2,8 @@ public class main {
 
 
     public static void main(String[] args) {
-        System.out.print("namaku farham");
-        System.out.printf("namaku %s%n","farham","18");
-        System.out.println("namaku farham");
+        System.out.print("farham");
+        System.out.printf("nama saya %s%n berumur %d%n","farham",18);
+        System.out.println(" Hai");
        }
 }
