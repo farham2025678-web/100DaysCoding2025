f0a87667815476c9682decb1c852eeebf470cb8b
farham2025678-web <farham2025678@gmail.com>
2025-09-10 17:48:48 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+// day 3 coding//
+System.out.println(" hai nama saya farham"); // ini adalah komentar//
+
+/*
+ini juga komentar tapi untuk kalimat yang lebih panjang ,bisa untuk lebih dari satu baris 
+*/
+ 
+	}
+}
