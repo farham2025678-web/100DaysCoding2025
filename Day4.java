548964d15fd2e5f558dfc60be3484b81e472931b
farham2025678-web <farham2025678@gmail.com>
2025-09-11 20:58:54 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		/*
+		\t untuk tab ,\n untuk enter \b untuk menghapus karakter sebelumnya 
+		
+		*/
+   /*
+   nah jadi kita tidak usah menggunakan banyak sout untuk baris baru kita tinggal menggunakan \n okey  
+  contoh nya seperti dibawah
+  */
+		
+		System.out.println(" halo\tselamat  malam \n  perkenalkan nama saya farham\n S\bsaya telah menghapus satu karakter sebelumnya ");
+	
+	}
+}
