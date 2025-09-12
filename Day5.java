b85a275145b836a65f839adc6ce0e20d94d06421
farham2025678-web <farham2025678@gmail.com>
2025-09-12 18:26:36 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println( "halo, dibawah ini ada empat tipe data bilangan  bulat dari yang terkecil sampai yang terbesar ");
+		byte  p= 10; 
+	   short l=1262;  
+		int      t =12345678;
+	long  j=1456789103;
+	System.out.println("panjang =" + p + "\nlebar = " + l + "\ntinggi  = " + t + "\nJarak = "  + j );
+	}
+}
