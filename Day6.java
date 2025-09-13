af8d2eed073f5b8da4660125a5026985b273deea
farham2025678-web <farham2025678@gmail.com>
2025-09-13 20:21:12 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		 float  p=  21.1f;
+		 double   l= 100.3;
+		 double ls= p*l;
+		 System.out.println("panjang = " + p +"\n lebar = " + l );
+		 System.out.println("luas = " + ls);
+		
+		
+	}
+}
