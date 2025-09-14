18f3d9a020146ee7d9c7490fd6cc07109d7b76dc
farham2025678-web <farham2025678@gmail.com>
2025-09-14 21:17:18 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		char i = 'f';
+		boolean b= true;
+		boolean s = false;
+		System.out.println("insial =  " + i +"\n nilai  benar = " + b + "\n nilai salah = " + s);
+		
+	}
+}
