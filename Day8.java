32c6e106f7131beef710aafa88ecee19d1532466
farham2025678-web <farham2025678@gmail.com>
2025-09-15 22:29:49 +0800

    Day8.java

--- Day8.java
+++ Day8.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 	String   teks = " halo perkenalkan nama saya farham saat ini saya sedang menggunakan tipe data String ";
-		System.out.println(teks );
+		System.out.println(teks);
 	}
 }
