2d7aac97de9f209d842158db867bd7ce6c2dd8e6
farham2025678-web <farham2025678@gmail.com>
2025-09-16 22:52:12 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		String nama ;
+		nama =  " farham";
+		System.out.println(" hai namaku = " + nama);
+		long  umur ;
+		umur = 18;
+		System.out.println(" saat ini  umurku = " +umur + " tahun");
+		
+	}
+}
