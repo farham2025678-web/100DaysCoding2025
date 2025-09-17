c716d6f9cd15b2367f7e0c0585b9450e65bf0438
farham2025678-web <farham2025678@gmail.com>
2025-09-17 15:53:55 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+ /*
+ variabel yang nilainya dapat berubah rubah contohnya seperti dibawah
+ */
+	    	    double  angka1 = 12.1;
+		System.out.println("angka1 = "+ angka1);
+			
+		angka1 = 200.1;
+		System.out.println("angka1 = " + angka1);
+		  /*
+		  konstanta  nilainya tidak dapat berubah rubah atau tetap contohnya seperti dibawah
+		  */
+		  
+		final double ANGKA1= 100.1;
+		System.out.println("konstanta = " + ANGKA1);
+		
+		
+	}
+}
