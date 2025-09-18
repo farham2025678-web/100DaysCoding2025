0371b2c4af98640fb22d3abf48c1917cfab587a6
farham2025678-web <farham2025678@gmail.com>
2025-09-18 16:22:34 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner biodata= new Scanner (System.in);
+		System.out.print(" nama = ");
+		String nama= biodata.next();
+		System.out.println(nama);
+		
+	}
+}
