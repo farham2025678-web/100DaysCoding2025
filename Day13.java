2096dd1d2a73aebf64bf5dd51604f78488ed0c3c
farham2025678-web <farham2025678@gmail.com>
2025-09-20 15:53:25 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    System.out.println(" Nama : Farham ");
+	    System.out.print(" umur : 18 tahun\t");
+	    System.out.printf("nama :%s,umur : %d,ipk : %f","farham" ,18,4.00);
+	
+	}
+}
