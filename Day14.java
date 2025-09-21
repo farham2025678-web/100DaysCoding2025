5e7082f03a4d2f27781671f3648b454aecc71b8f
farham2025678-web <farham2025678@gmail.com>
2025-09-21 16:44:34 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,23 @@
+public class Main {
+	public static void main(String[] args) {
+		/*
+String ke byte = Byte.parseByte
+               short = Short.parseShort
+		            int = Integer.parseInt
+        		 long = Long.parseLong
+            double = Double.parseDouble
+            float     =  Float.parseFloat
+		bolean =Boolean.parseBoolean
+		saya kasih  2 contoh dibawah 
+		*/
+		//string ke int
+		String angkaString = "12";
+		System.out.println(" hasil konversi nya untuk  ke int :" + Integer.parseInt(angkaString));
+		//String ke double
+		String desimalString = "12.5";
+		System.out.println("hasil konversinya ke  double : "+ Double.parseDouble(desimalString));
+		
+		
+			
+	}
+}
