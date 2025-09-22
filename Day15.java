57d611c0dd711f3be364bc41017bf1b3f19e4883
farham2025678-web <farham2025678@gmail.com>
2025-09-22 21:17:25 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a= 12;
+		int b= 5;
+		long penjumlahan = a+b;
+		int pengurangan = a - b;
+		System.out.println("hasil penjumlahan = " + penjumlahan);
+		System.out.println("hasil pengurangan = " + pengurangan);
+	
+	}
+}
