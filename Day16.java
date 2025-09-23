af695d42a9512eb2ffb94cafd0e98291113236b7
farham2025678-web <farham2025678@gmail.com>
2025-09-23 21:29:24 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=  10;
+		int b= 5;
+		long perkalian = a*b;
+		int pembagian = a /b;
+		System.out.println("hasil perkalian = " + perkalian);
+		System.out.println("hasil pembagian = " + pembagian);
+	
+	}
+}
