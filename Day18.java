a1c3b698dc394c66ef031258a8b3c1853cf70bb2
farham2025678-web <farham2025678@gmail.com>
2025-09-25 21:14:07 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		byte  a = 100;
+		 int b = a; // konversi byte ke int
+		 
+		 System.out.println( " nilai byte :" + a);
+		 System.out.println("nilai konversinya ke int : "+b);
+		
+	}
+}
