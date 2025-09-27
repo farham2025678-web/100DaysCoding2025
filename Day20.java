50bab3f157a464cbf3f61b834268ccaf357754a9
farham2025678-web <farham2025678@gmail.com>
2025-09-27 20:50:08 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+		//cara pertama
+		int a= 995;
+String   b  = String.valueOf(a); 
+// cara kedua
+double d  = 6.12;
+String  e   = Double.toString(d);
+ //cara ketiga
+ long l = 123;
+ String s = " " + l ;
+ System.out.println(b);
+ System.out.println(e);
+ System.out.println(s);
+ 
+
+
+
+
+	}
+}
