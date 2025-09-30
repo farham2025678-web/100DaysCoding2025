1cd4c5a3e1f9f46bcde8d1a62f4586fddbcfe868
farham2025678-web <farham2025678@gmail.com>
2025-09-30 20:20:44 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+	  double  panjang = 11.5;
+	  double lebar = 5.1;
+	  double luas = panjang*lebar;
+	  System.out.println("luas persegi panjang :" + luas);
+	}
+}
