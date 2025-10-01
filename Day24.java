37a165029cf2e11333a98dfdc5fa6d298489be25
farham2025678-web <farham2025678@gmail.com>
2025-10-01 20:41:01 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		double JariJari = 10.5;
+		
+		double luas = Math.PI*JariJari * JariJari;
+		
+		System.out.println("luas lingkaran :" + luas);
+	
+
+	}
+}
