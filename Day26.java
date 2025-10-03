530b9ea96f58fb285de3e9ced588d6178c81a7fb
farham2025678-web <farham2025678@gmail.com>
2025-10-03 20:36:57 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		int angka = 12;
+		int a  = angka+=2;
+		int b = angka-=2;
+		int c = angka*=2;
+		int d= angka/=2;
+		int e = angka%=2;
+		System.out.println("penambahan :"+a);
+		System.out.println("pengurangan :"+ b);
+		System.out.println("perkalian:"+ c);
+		System.out.println("pembagian :"+ d);
+		System.out.println("modulus :"+ e);
+		
+		
+		
+	}
+}
