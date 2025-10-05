d02714b7f10f4920e9cbe1c24c717050d16bab05
farham2025678-web <farham2025678@gmail.com>
2025-10-05 20:37:30 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka1= 2;
+        int angka2= 2;
+        int angka3 = 6;
+
+        
+        System.out.println("angka1==angka2"+(angka1==angka2));
+        System.out.println("angka1==angka3"+(angka1==angka3));
+
+       
+        System.out.println("angka1!=angka2"+(angka1!=angka2));
+        System.out.println("angka1!=angka3"+(angka1!=angka3));
+
+         
+    }
+}
