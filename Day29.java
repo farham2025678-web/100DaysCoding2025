bb4a10ea0a145de5f95a727255263c17c5deba23
farham2025678-web <farham2025678@gmail.com>
2025-10-06 20:22:33 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,13 @@
+  public class Main {
+     public static void main(String[] args) {
+        int f= 2;
+        int i= 6;
+
+        // Lebih kecil dari (<)
+        System.out.println("f < i: " + (f < i)); // true
+
+        // Lebih besar dari (>)
+        System.out.println("f > i: " + (f > i)); // false
+        
+    }
+		}
