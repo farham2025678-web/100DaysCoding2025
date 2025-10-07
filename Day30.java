81dab0d77577e079ccc1ff1e89c760152b873f0d
farham2025678-web <farham2025678@gmail.com>
2025-10-07 22:21:02 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 22;
+		int b=  22;
+		int c=   6;
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a>=c);
+		System.out.println(a<=c);
+	}
+}
