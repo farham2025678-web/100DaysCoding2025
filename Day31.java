589b45a45d18e103edb35ec08fd5afbb793252e5
farham2025678-web <farham2025678@gmail.com>
2025-10-08 21:53:22 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = true;
+		boolean c = false;
+		System.out.println(a&&b);
+		System.out.println(a&&c);
+	}
+}
