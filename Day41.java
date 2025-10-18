341d41e1cd95fb65541d3c341825b40d6a2ae10b
farham2025678-web <farham2025678@gmail.com>
2025-10-18 21:30:29 +0800

    Day41.java

---
+++ Day41.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		String a = "150";
+		String b= "200";
+		int c = Integer.parseInt(a);
+		int  d = Integer.parseInt(b);
+		int e = c+d;
+		System.out.println("jumlah peserta = "+e);
+	     
+	
+	}
+	
+}
