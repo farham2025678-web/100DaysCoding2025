fe53fbd554bba44fa0c4d1ead790c85bd7078c0c
farham2025678-web <farham2025678@gmail.com>
2025-10-27 18:47:36 +0800

    Day50.java

---
+++ Day50.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai: ");
+        int angka = scanner.nextInt();
+
+        String angka = nilai %2==0? "genap" : "Ganjil";
+        System.out.println(angka);
+    }
+
+	
+}
