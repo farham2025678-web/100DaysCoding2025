ffbb99cdd0204cce411c9ddb15ef2d3a9cbbe4e0
farham2025678-web <farham2025678@gmail.com>
2025-10-30 21:27:38 +0800

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20;i++) {
+            System.out.println("Angka: " + i);
+            if (i == 10){
+                System.out.println("Berhenti di angka 20!");
+                break;
+            }
+        }
+        
+    }
