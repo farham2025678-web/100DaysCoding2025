0d4fc81f3dcc1ec2b18393a13b47d264a474d5d7
farham2025678-web <farham2025678@gmail.com>
2025-11-13 21:00:30 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int lebar = 5;
+        for (int i = 1; i <= lebar; i++) {
+            System.out.print("* ");
+        }
+    }
+}
