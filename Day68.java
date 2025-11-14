7e6617dd77069a1e45b329857286eb953f6b8081
farham2025678-web <farham2025678@gmail.com>
2025-11-14 22:16:03 +0800

    Day68.java

---
+++ Day68.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int ukuran = 4;
+        for (int i = 1; i <= ukuran; i++) {
+            for (int j = 1; j <= ukuran; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
