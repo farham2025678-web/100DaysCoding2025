328c07ef215738978b344e4c294c0843306db6b8
farham2025678-web <farham2025678@gmail.com>
2025-11-19 22:23:08 +0800

    Day73.java

---
+++ Day73.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jumlah = 0;
+
+        while (true) {
+            System.out.print("Masukkan angka pertama : ");
+            int angka1 = scanner.nextInt();
+            if (angka1 < 0) break;
+
+            System.out.print("Masukkan angka kedua : ");
+            int angka2 = scanner.nextInt();
+            if (angka2 < 0) break;
+
+            jumlah = angka1 + angka2;
+            System.out.println("Hasil penjumlahan:  " + jumlah);
+        }
+    }
+}
