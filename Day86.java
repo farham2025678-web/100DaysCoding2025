0f71dd90eaa64d3881752d00a1f4347728a17a18
farham2025678-web <farham2025678@gmail.com>
2025-12-02 22:56:38 +0800

    Day86.java

--- Day86.java
+++ Day86.java
@@ -1,13 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
 		int []a= {1,7,4,5,10};
-		int minimum=a[0];
+		int minimal=a[0];
 		for(int i = 0; i < a.length;i++){
-			if(a[i]<minimum){
-				minimum=a[i];
+			if(a[i]<minimal){
+				minimal=a[i];
 		}
 	}
 	
-System.out.println(minimum);
+System.out.println(minimal);
 }
 }
