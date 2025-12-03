734332be5659b5b480359656ee09cdea161260b7
farham2025678-web <farham2025678@gmail.com>
2025-12-03 19:56:59 +0800

    Day87.java

--- Day87.java
+++ Day87.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
          Scanner input= new Scanner(System.in);
-         int[] array = {1,2,4,5,6,7,8,9,10};
+         int[] array = {1,2,3,4,5,6,7,8,9,10};
 
         System.out.print("Masukkan nilai yang ingin dicari: ");
         int target = input.nextInt();
