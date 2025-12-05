be80a0caa1af051655b6f3fe5a37ef71233067d9
farham2025678-web <farham2025678@gmail.com>
2025-12-05 20:24:40 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,18 @@
+public class main{
+    public static void main(String[] args) {
+       
+        cetakHalo();
+        cetakNama("farham");
+        
+    }
+
+ 
+    public static void cetakHalo() {
+        System.out.println("Halo, dunia!");
+    }
+    public static void cetakNama(String nama) {
+        System.out.println("Nama saya adalah " + nama);
+    
+
+    }
+}
