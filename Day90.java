5bf7424e23516a632f42f22e4a2965e6f118b238
farham2025678-web <farham2025678@gmail.com>
2025-12-06 21:01:14 +0800

    Day90.java

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int hasil = tambah(5, 3);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+}
