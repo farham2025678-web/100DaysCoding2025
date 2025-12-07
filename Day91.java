c6c1836ca44e41457bff78bc1feaa4ebc3f93d2e
farham2025678-web <farham2025678@gmail.com>
2025-12-07 19:29:12 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String[] args) {
+        hitungLuas(5, 3);
+        
+    }
+
+    public static void hitungLuas(int panjang, int lebar) {
+        int luas = panjang * lebar;
+        System.out.println("Luas persegi panjang dengan panjang   adalah: " + luas);
+    }
+}
