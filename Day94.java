e07b580638a266ca732b8ef4dc8263f9113fbbe1
farham2025678-web <farham2025678@gmail.com>
2025-12-10 20:46:57 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai desimal: ");
+        double nilai = scanner.nextDouble();
+
+        cetakHasil(nilai);
+    }
+
+    public static void cetakHasil(double nilai) {
+        System.out.println("Ceil: " + Math.ceil(nilai));
+        System.out.println("Floor: " + Math.floor(nilai));
+        System.out.println("Round: " + Math.round(nilai));
+    }
+}
