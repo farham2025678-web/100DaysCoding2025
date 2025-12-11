9f8d687f2a6cf2578d4cb3b50a031fbad6d55b8c
farham2025678-web <farham2025678@gmail.com>
2025-12-11 20:08:50 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        cetakPesan("Halo, dunia!");
+        
+    }
+
+    public static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+}
